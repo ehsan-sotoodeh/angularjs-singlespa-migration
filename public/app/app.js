f637fda2298af715d83a95b2944e3a0af9500c6a
Ehsan Sotoodeh <esotoodeh@coppertreeanalytics.com>
2022-01-21 17:03:31 -0800

    finished the router

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,36 @@
-'use strict';
-var app = angular.module('AngularSingleSpa', ['ngRoute']).config(function($routeProvider){
-  $routeProvider
-    .when("/home",{
-      templateUrl: "assets/templates/home.html",
-      controller:"homeController"
-    })
-})
+"use strict";
 
-app.run(['$q', '$rootScope', '$timeout', function($q, $rootScope, $timeout) {
-  $rootScope.loading = true;
-}]);
+var app = angular
+  .module("AngularSingleSpa", ["ngRoute"])
+  .directive("displaySinglespa", [
+    function () {
+      return {
+        restrict: "E",
+        templateUrl: "assets/templates/display-singlespa.template.html",
+      };
+    },
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider.when("/", {
+      templateUrl: "assets/templates/home.template.html",
+      controller: "homeController",
+    })
+    $routeProvider.when("/pageOne", {
+      templateUrl: "assets/templates/pageOne.template.html",
+      controller: "pageOneController",
+    })
+    $routeProvider.when("/pageTwo", {
+      templateUrl: "assets/templates/pageTwo.template.html",
+      controller: "pageTwoController",
+    })
+    
+    ;
+  })
+  .run([
+    "$q",
+    "$rootScope",
+    "$timeout",
+    function ($q, $rootScope, $timeout) {
+      $rootScope.loading = true;
+    },
+  ]);
---
+++ public/app/controllers/pageTwoController.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// SingleSpa Controller
+app.controller('pageTwoController', function($scope) {
+  //variable to prevent multiple playloops
+    $scope.message = "Hello World from pageTwoController +++++++++ ";
+});
